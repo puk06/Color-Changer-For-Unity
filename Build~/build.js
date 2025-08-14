0539ecb2aba7904e021bd7923a1f21b9f275eb82
Puk06 <86549420+puk06@users.noreply.github.com>
2025-08-14 11:54:04 +0900

    build.jsの書き直し

--- Build~/build.js
+++ Build~/build.js
@@ -1,14 +1,14 @@
 const fs = require("node:fs");
-const archiver = require('archiver');
+const archiver = require("archiver");
 const path = require("node:path");
 
-const PACKAGE_JSON = path.join(__dirname, "../PackageData", 'package.json');
+const PACKAGE_JSON = path.join(__dirname, "../", "package.json");
 const packageJson = JSON.parse(fs.readFileSync(PACKAGE_JSON, "utf8"));
 
 const PACKAGE_NAME = packageJson.name;
 const PACKAGE_VERSION = packageJson.version;
 
-const BUILD_DIR = path.join(__dirname, 'build');
+const BUILD_DIR = path.join(__dirname, "build");
 if (!fs.existsSync(BUILD_DIR)) {
     fs.mkdirSync(BUILD_DIR);
 }
@@ -19,17 +19,18 @@ if (fs.existsSync(ZIP_FILE_NAME)) {
 }
 
 const output = fs.createWriteStream(ZIP_FILE_NAME);
-const archive = archiver('zip', { zlib: { level: 9 } });
+const archive = archiver("zip", { zlib: { level: 9 } });
 
-archive.glob('**/*', {
-    cwd: path.join(__dirname, '../PackageData')
+archive.glob("**/*", {
+    cwd: path.join(__dirname, "../"),
+    ignore: ["Build~/**"]
 });
 
-output.on('close', () => {
+output.on("close", () => {
     console.log(`ZIP作成完了: ${archive.pointer()} total bytes`);
 });
 
-archive.on('error', err => { throw err; });
+archive.on("error", err => { throw err; });
 
 archive.pipe(output);
 
