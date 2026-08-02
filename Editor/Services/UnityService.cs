331cd4aa63e0a19b5ad248a56d1b829ce84fd6d9
codefactor-io <support@codefactor.io>
2026-08-02 00:47:09 +0000

    [CodeFactor] Apply fixes

--- Editor/Services/UnityService.cs
+++ Editor/Services/UnityService.cs
@@ -46,7 +46,7 @@ namespace net.puk06.ColorChanger.Editor.Services
             var titleSize = style.CalcSize(new GUIContent(title));
 
             float lineX = rect.x + titleSize.x + 8f;
-            float lineY = rect.y + rect.height / 2f;
+            float lineY = rect.y + (rect.height / 2f);
 
             EditorGUI.DrawRect(
                 new Rect(lineX, lineY, rect.width - titleSize.x - 10f, lineThickness),
